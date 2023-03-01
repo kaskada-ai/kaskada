de7d09d541e50c4a1a528e934738604b5ac34f34
Ben Chambers <ben.chambers@datastax.com>
2023-03-01 13:31:41 -0800

    comment

--- crates/sparrow-qfr-tool/src/chrome_tracing/stack_frame.rs
+++ crates/sparrow-qfr-tool/src/chrome_tracing/stack_frame.rs
@@ -2,6 +2,8 @@ use serde::Serialize;
 
 #[derive(Serialize, Eq, PartialEq, Debug)]
 pub(crate) struct StackFrame {
+    // The ID is omitted because it is included when serializing
+    // the frames in a trace (as a map from ID to the remaining fields).
     #[serde(skip_serializing)]
     pub(crate) id: StackFrameId,
     #[serde(skip_serializing_if = "str::is_empty")]
