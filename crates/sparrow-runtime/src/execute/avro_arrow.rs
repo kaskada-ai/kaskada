62314933449a67b2f9d32f106f1531c0745f64e4
Jonathan Ellis <jbellis@datastax.com>
2023-03-16 17:22:14 -0400

    add support for avro long, float, timestampmicros, string

--- crates/sparrow-runtime/src/execute/avro_arrow.rs
+++ crates/sparrow-runtime/src/execute/avro_arrow.rs
@@ -1,7 +1,7 @@
 use avro_rs::types::Value;
-use arrow::array::{ArrayRef, ArrowPrimitiveType, PrimitiveArray};
+use arrow::array::{ArrayRef, ArrowPrimitiveType, PrimitiveArray, StringBuilder};
 use arrow::error::ArrowError;
-use arrow::datatypes::{Float64Type, Int32Type, TimestampMillisecondType};
+use arrow::datatypes::{Date32Type, Date64Type, DurationSecondType, Float16Type, Float32Type, Float64Type, Int16Type, Int32Type, Int64Type, Int8Type, TimestampMicrosecondType, TimestampMillisecondType, UInt16Type, UInt32Type, UInt64Type, UInt8Type, Utf8Type};
 use std::sync::Arc;
 
 pub fn avro_to_arrow(values: Vec<Vec<(String, Value)>>) -> Result<Vec<ArrayRef>, ArrowError> {
@@ -39,6 +39,56 @@ where
         .map(|e| Arc::new(e) as ArrayRef)
 }
 
+fn build_avro_utf8_array(
+    values: &Vec<Vec<(String, Value)>>,
+    field_index: usize,
+) -> Result<ArrayRef, ArrowError> {
+    let mut builder = StringBuilder::new();
+
+    for row in values {
+        let (_, value) = &row[field_index];
+        match value {
+            Value::String(s) => builder.append_value(s),
+            Value::Null => builder.append_null(),
+            _ => {
+                return Err(ArrowError::ParseError(format!(
+                    "Error while parsing value {:?} for column {}",
+                    value, field_index
+                )));
+            }
+        };
+    }
+
+    Ok(Arc::new(builder.finish()) as ArrayRef)
+}
+
+fn avro_to_arrow_field(
+    values: &Vec<Vec<(String, Value)>>,
+    field_index: usize,
+) -> Result<ArrayRef, ArrowError> {
+    let _t = TimestampMillisecondType::DATA_TYPE;
+    // Infer the Arrow type based on the Avro type of the first value in the column.
+    let (_, first_value) = &values[0][field_index];
+    match first_value {
+        Value::Int(_) => build_avro_primitive_array::<Int32Type>(&values, field_index),
+        Value::Long(_) => build_avro_primitive_array::<Int64Type>(&values, field_index),
+        Value::Float(_) => build_avro_primitive_array::<Float32Type>(&values, field_index),
+        Value::Double(_) => build_avro_primitive_array::<Float64Type>(&values, field_index),
+        Value::TimestampMillis(_) => {
+            build_avro_primitive_array::<TimestampMillisecondType>(&values, field_index)
+        },
+        Value::TimestampMicros(_) => {
+            build_avro_primitive_array::<TimestampMicrosecondType>(&values, field_index)
+        },
+        Value::Date(_) => build_avro_primitive_array::<Date32Type>(&values, field_index),
+        Value::String(_) => build_avro_utf8_array(&values, field_index),
+        _ => Err(ArrowError::ParseError(format!(
+            "Unsupported Avro type {:?} for column {:?}",
+            first_value, field_index
+        ))),
+    }
+}
+
 pub trait AvroParser: ArrowPrimitiveType {
     fn parse_avro_value(value: &Value) -> Option<Self::Native>;
 }
@@ -47,6 +97,24 @@ impl AvroParser for Int32Type {
     fn parse_avro_value(value: &Value) -> Option<Self::Native> {
         match value {
             Value::Int(i) => Some(*i),
+            _ => None,
+        }
+    }
+}
+
+impl AvroParser for Int64Type {
+    fn parse_avro_value(value: &Value) -> Option<Self::Native> {
+        match value {
+            Value::Long(l) => Some(*l),
+            _ => None,
+        }
+    }
+}
+
+impl AvroParser for Float32Type {
+    fn parse_avro_value(value: &Value) -> Option<Self::Native> {
+        match value {
+            Value::Float(f) => Some(*f),
             _ => None,
         }
     }
@@ -70,22 +138,20 @@ impl AvroParser for TimestampMillisecondType {
     }
 }
 
-fn avro_to_arrow_field(
-    values: &Vec<Vec<(String, Value)>>,
-    field_index: usize,
-) -> Result<ArrayRef, ArrowError> {
-    let _t = TimestampMillisecondType::DATA_TYPE;
-    // Infer the Arrow type based on the Avro type of the first value in the column.
-    let (_, first_value) = &values[0][field_index];
-    match first_value {
-        Value::Int(_) => build_avro_primitive_array::<Int32Type>(&values, field_index),
-        Value::Double(_) => build_avro_primitive_array::<Float64Type>(&values, field_index),
-        Value::TimestampMillis(_) => {
-            build_avro_primitive_array::<TimestampMillisecondType>(&values, field_index)
+impl AvroParser for TimestampMicrosecondType {
+    fn parse_avro_value(value: &Value) -> Option<Self::Native> {
+        match value {
+            Value::TimestampMicros(t) => Some(*t),
+            _ => None,
         }
-        _ => Err(ArrowError::ParseError(format!(
-            "Unsupported Avro type {:?} for column {:?}",
-            first_value, field_index
-        ))),
+    }
+}
+
+impl AvroParser for Date32Type {
+    fn parse_avro_value(value: &Value) -> Option<Self::Native> {
+        match value {
+            Value::Date(d) => Some(*d),
+            _ => None,
+        }
     }
 }
