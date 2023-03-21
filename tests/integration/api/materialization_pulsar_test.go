871d16171043a7e22fd68fce99496cd30e434eee
Jordan Frazier <jordan.frazier@datastax.com>
2023-03-20 17:57:54 -0700

    integ test

---
+++ tests/integration/api/materialization_pulsar_test.go
@@ -0,0 +1,171 @@
+package api_test
+
+import (
+	"context"
+
+	"github.com/apache/pulsar-client-go/pulsar"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	v1alpha "github.com/kaskada-ai/kaskada/gen/proto/go/kaskada/kaskada/v1alpha"
+	helpers "github.com/kaskada-ai/kaskada/tests/integration/shared/helpers"
+	. "github.com/kaskada-ai/kaskada/tests/integration/shared/matchers"
+)
+
+var _ = FDescribe("Materialization with Pulsar upload", Ordered, Label("pulsar"), func() {
+	var (
+		ctx                   context.Context
+		cancel                context.CancelFunc
+		conn                  *grpc.ClientConn
+		pulsarClient          pulsar.Client
+		table                 *v1alpha.Table
+		tableClient           v1alpha.TableServiceClient
+		tableName             string
+		topicUrl              string
+		materializationClient v1alpha.MaterializationServiceClient
+	)
+
+	BeforeAll(func() {
+		//get connection to wren
+		ctx, cancel, conn = grpcConfig.GetContextCancelConnection(10)
+		ctx = metadata.AppendToOutgoingContext(ctx, "client-id", *integrationClientID)
+
+		// get a grpc client for the table & materialization services
+		tableClient = v1alpha.NewTableServiceClient(conn)
+		materializationClient = v1alpha.NewMaterializationServiceClient(conn)
+
+		// create a pulsar client
+		pulsarClient, _ = pulsar.NewClient(pulsar.ClientOptions{
+			URL: "pulsar://localhost:6650",
+		})
+		// Expect(err).Should(BeNil())
+		defer pulsarClient.Close()
+
+		// create a table
+		tableName = "pulsar_table"
+		table = &v1alpha.Table{
+			TableName:           tableName,
+			TimeColumnName:      "purchase_time",
+			EntityKeyColumnName: "customer_id",
+			SubsortColumnName: &wrapperspb.StringValue{
+				Value: "subsort_id",
+			},
+		}
+		_, err := tableClient.CreateTable(ctx, &v1alpha.CreateTableRequest{Table: table})
+		Expect(err).ShouldNot(HaveOccurredGrpc())
+
+		// load data into the table
+		helpers.LoadTestFileIntoTable(ctx, conn, table, "purchases/purchases_part1.parquet")
+	})
+
+	AfterAll(func() {
+		// clean up items from the test
+		materializationClient.DeleteMaterialization(ctx, &v1alpha.DeleteMaterializationRequest{MaterializationName: "purchase_min_and_max"})
+		// this materialization might not have been created if test had an issue, so we don't check error here
+		_, err := tableClient.DeleteTable(ctx, &v1alpha.DeleteTableRequest{TableName: tableName})
+		Expect(err).ShouldNot(HaveOccurredGrpc())
+
+		cancel()
+		conn.Close()
+	})
+
+	Describe("Create a materialization", func() {
+		It("Should work without error", func() {
+			createRequest := &v1alpha.CreateMaterializationRequest{
+				Materialization: &v1alpha.Materialization{
+					MaterializationName: "purchase_min_and_max",
+					Expression: `
+{
+time: pulsar_table.purchase_time,
+key: pulsar_table.customer_id,
+max_amount: pulsar_table.amount | max(),
+min_amount: pulsar_table.amount | min(),
+}`,
+					// TODO: FRAZ - create issue for normalizing the mat dest protos
+					Destination: &v1alpha.Materialization_Destination{
+						Destination: &v1alpha.Materialization_Destination_Pulsar{
+							Pulsar: &v1alpha.PulsarDestination{
+								BrokerServiceUrl: "pulsar://pulsar:6650",
+								Tenant:           "public",
+								Namespace:        "default",
+								TopicName:        "my_topic",
+							},
+						},
+					},
+				},
+			}
+			topicUrl = "persistent://public/default/my_topic"
+			res, err := materializationClient.CreateMaterialization(ctx, createRequest)
+			Expect(err).ShouldNot(HaveOccurredGrpc())
+			Expect(res).ShouldNot(BeNil())
+			VerifyRequestDetails(res.RequestDetails)
+		})
+
+		It("Should upload results to pulsar", func() {
+			Eventually(func(g Gomega) {
+
+				consumer, err := pulsarClient.Subscribe(pulsar.ConsumerOptions{
+					Topic:            topicUrl,
+					SubscriptionName: "my-subscription",
+					Type:             pulsar.Shared,
+				})
+				g.Expect(err).Should(BeNil())
+				defer consumer.Close()
+
+				// Since we don't know the schema and can't create a struct
+				// to unmarshal data into, we can ask the consumer to auto consume
+				// and decode automatically.
+				schema, err := consumer.Schema()
+				g.Expect(err).Should(BeNil())
+
+				for i := 0; i < 3; i++ {
+					msg, err := consumer.Receive(context.Background())
+					g.Expect(err).Should(BeNil())
+
+					data, err := schema.Decode(msg.Payload())
+					g.Expect(err).Should(BeNil())
+
+					// Verify the message fields
+					g.Expect(data["time"].(int64)).Should(Equal(1578182400000000000))
+					g.Expect(data["key"].(string)).Should(Equal("karen"))
+					g.Expect(data["max_amount"].(int64)).Should(Equal(10))
+
+					consumer.Ack(msg)
+
+					time.Sleep(1 * time.Second) // add a delay for testing purposes
+				}
+
+				// dataType, shape, values, err := pulsarClient.TensorGetValues("karen")
+				// g.Expect(err).ShouldNot(HaveOccurred())
+				// g.Expect(dataType).Should(Equal("INT64"))
+				// g.Expect(shape).Should(Equal([]int64{1, 3}))
+				// g.Expect(values).Should(Equal([]int64{1578182400000000000, 9, 2}))
+
+				// dataType, shape, values, err = redisAIClient.TensorGetValues("patrick")
+				// g.Expect(err).ShouldNot(HaveOccurred())
+				// g.Expect(dataType).Should(Equal("INT64"))
+				// g.Expect(shape).Should(Equal([]int64{1, 3}))
+				// g.Expect(values).Should(Equal([]int64{1578182400000000000, 5000, 3}))
+
+				// _, _, _, err = redisAIClient.TensorGetValues("spongebob")
+				// g.Expect(err).Should(HaveOccurred())
+				// g.Expect(err.Error()).Should(ContainSubstring("tensor key is empty"))
+			}, "30s", "1s").Should(Succeed())
+		})
+	})
+
+	Describe("Load the second file into the table", func() {
+		It("Should work without error", func() {
+			helpers.LoadTestFileIntoTable(ctx, conn, table, "purchases/purchases_part2.parquet")
+		})
+
+		It("Should upload results to pulsar", func() {
+			Eventually(func(g Gomega) {
+				// hello
+			}, "30s", "1s").Should(Succeed())
+		})
+	})
+})
--- wren/go.mod
+++ wren/go.mod
@@ -12,7 +12,7 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware/providers/zerolog/v2 v2.0.0-rc.2.0.20211203031854-ed43ab3fa4f8
 	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.0-rc.2.0.20211203031854-ed43ab3fa4f8
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.3
 	github.com/kouhin/envflag v0.0.0-20150818174321-0e9a86061649
 	github.com/mattn/go-sqlite3 v1.14.14
 	github.com/onsi/ginkgo/v2 v2.1.4
@@ -28,17 +28,17 @@ require (
 	go.opentelemetry.io/otel/sdk v1.2.0
 	go.opentelemetry.io/otel/trace v1.2.0
 	golang.org/x/sync v0.1.0
-	google.golang.org/genproto v0.0.0-20230221151758-ace64dc21148
-	google.golang.org/grpc v1.53.0
+	google.golang.org/grpc v1.52.0-dev
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
-	cloud.google.com/go v0.107.0 // indirect
-	cloud.google.com/go/compute v1.18.0 // indirect
-	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/iam v0.11.0 // indirect
-	cloud.google.com/go/storage v1.27.0 // indirect
+	cloud.google.com/go v0.82.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.0 // indirect
+	cloud.google.com/go/storage v1.15.0 // indirect
+	github.com/99designs/go-keychain v0.0.0-20191008050251-8e49817e8af4 // indirect
+	github.com/99designs/keyring v1.2.1 // indirect
+	github.com/AthenZ/athenz v1.10.39 // indirect
 	github.com/Azure/azure-pipeline-go v0.2.3 // indirect
 	github.com/Azure/azure-storage-blob-go v0.15.0 // indirect
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
@@ -47,20 +47,38 @@ require (
 	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
 	github.com/Azure/go-autorest/logger v0.2.1 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/DataDog/zstd v1.5.0 // indirect
+	github.com/apache/pulsar-client-go v0.9.0 // indirect
+	github.com/ardielle/ardielle-go v1.5.2 // indirect
+	github.com/danieljoos/wincred v1.1.2 // indirect
+	github.com/dvsekhvalnov/jose2go v1.5.0 // indirect
 	github.com/envoyproxy/protoc-gen-validate v0.9.1 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
+	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/pprof v0.0.0-20210720184732-4bb14d4b1be1 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
-	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
+	github.com/gsterjov/go-libsecret v0.0.0-20161001094733-a6f4afe4910c // indirect
+	github.com/klauspost/compress v1.14.4 // indirect
+	github.com/linkedin/goavro/v2 v2.9.8 // indirect
 	github.com/mattn/go-ieproxy v0.0.9 // indirect
+	github.com/mtibben/percent v0.2.1 // indirect
+	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/oauth2 v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
 	golang.org/x/tools v0.3.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
-	google.golang.org/api v0.110.0 // indirect
+	google.golang.org/api v0.47.0 // indirect
 )
 
 require (
@@ -69,7 +87,6 @@ require (
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/c2fo/vfs/v6 v6.6.0
 	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -79,7 +96,6 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.10.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/kaskada-ai/kaskada/gen/proto/go v0.0.0-00010101000000-000000000000
 	github.com/lib/pq v1.10.6
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
