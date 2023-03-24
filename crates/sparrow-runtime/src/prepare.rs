e4103680d5cd13d3e58cefe939b52af875e17de9
Jordan Frazier <jordan.frazier@datastax.com>
2023-03-24 09:06:31 -0700

    Fix clippy warrnings

--- crates/sparrow-runtime/src/prepare.rs
+++ crates/sparrow-runtime/src/prepare.rs
@@ -65,7 +65,7 @@ pub fn prepared_batches(
                 reader_from_csv(config, reader, prepare_hash, slice)?
             }
             source_data::Source::PulsarSubscription(ps) => {
-                reader_from_pulsar(config, &ps, prepare_hash, slice)?
+                reader_from_pulsar(config, ps, prepare_hash, slice)?
             }
         },
     };
