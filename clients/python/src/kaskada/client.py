bc298bbcb3b2d03ec76043f8c3429bcfbd1c7461
Ryan Michael <kerinin@gmail.com>
2023-04-19 11:10:33 -0400

    Updates from second trial
    * Explain how to know if you need pip or pip3 in the “tip”
    * Remove redundant title in table loading warning.
    * Change titles involved with loading to make it clear that you’re loading into a table
    * Output the log location when starting a subprocess in Python
    * Fix method names “load_file -> load”
    * Improve “client must be provided” error to suggest creating a session
    * Add comments to arguments in all examples clarifying the argument’s meaning
    * Reduce use of Pandas where possible
    * Remove use of “search” parameter in table list call (or move to a subsection related to searching)

--- clients/python/src/kaskada/client.py
+++ clients/python/src/kaskada/client.py
@@ -159,11 +159,9 @@ def get_client(client: Optional[Client] = None) -> Client:
         Client: The provided client argument or the global client.
     """
     if client is None and KASKADA_DEFAULT_CLIENT is None:
-        raise ValueError("Client must be provided")
+        raise ValueError("No client was provided, and no global client is configured. Consider setting the global client by creating a session, for example 'kaskada.api.session.LocalBuilder().build()`.")
     elif client is None:
         client = KASKADA_DEFAULT_CLIENT
 
-    if client is None:
-        raise ValueError("Client must be provided")
     validate_client(client)
     return client
