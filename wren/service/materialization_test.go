51de38d95fb4b04e6fc902503394ba20469c5343
Eric Pinzur <epinzur@gmail.com>
2023-05-16 05:20:32 +0200

    more progress

--- wren/service/materialization_test.go
+++ wren/service/materialization_test.go
@@ -2,146 +2,126 @@ package service
 
 import (
 	"context"
-	"fmt"
-	"reflect"
-	"testing"
 
 	v1alpha "github.com/kaskada-ai/kaskada/gen/proto/go/kaskada/kaskada/v1alpha"
 	"github.com/kaskada-ai/kaskada/wren/compute"
 	"github.com/kaskada-ai/kaskada/wren/ent"
-	"github.com/kaskada-ai/kaskada/wren/internal"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	mock "github.com/stretchr/testify/mock"
 )
 
-func Test_materialization_create(t *testing.T) {
-	objectStoreDestination := &v1alpha.Destination{
-		Destination: &v1alpha.Destination_ObjectStore{
-			ObjectStore: &v1alpha.ObjectStoreDestination{
-				FileType:        v1alpha.FileType_FILE_TYPE_CSV,
-				OutputPrefixUri: "gs://some-bucket/some-prefix",
+var _ = Describe("MaterializationService", func() {
+	var (
+		owner                  *ent.Owner
+		objectStoreDestination = &v1alpha.Destination{
+			Destination: &v1alpha.Destination_ObjectStore{
+				ObjectStore: &v1alpha.ObjectStoreDestination{
+					FileType:        v1alpha.FileType_FILE_TYPE_CSV,
+					OutputPrefixUri: "gs://some-bucket/some-prefix",
+				},
 			},
-		},
-	}
-	pulsarDestination := &v1alpha.Destination{
-		Destination: &v1alpha.Destination_Pulsar{
-			Pulsar: &v1alpha.PulsarDestination{
-				Config: &v1alpha.PulsarConfig{},
+		}
+		pulsarDestination = &v1alpha.Destination{
+			Destination: &v1alpha.Destination_Pulsar{
+				Pulsar: &v1alpha.PulsarDestination{
+					Config: &v1alpha.PulsarConfig{},
+				},
 			},
-		},
-	}
+		}
+	)
 
-	type fields struct {
-		v1alpha.UnimplementedMaterializationServiceServer
-		kaskadaTableClient    internal.KaskadaTableClient
-		kaskadaViewClient     internal.KaskadaViewClient
-		dataTokenClient       internal.DataTokenClient
-		materializationClient internal.MaterializationClient
-		computeManager        compute.ComputeManager
-	}
-	type args struct {
-		owner                       *ent.Owner
-		createMaterailzationRequest *v1alpha.CreateMaterializationRequest
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *v1alpha.CreateMaterializationResponse
-		wantErr bool
-		err     error
-	}{
-		{
-			name: "missing materialization should throw error",
-			fields: fields{
-				UnimplementedMaterializationServiceServer: v1alpha.UnimplementedMaterializationServiceServer{},
-			},
-			args: args{
-				owner:                       &ent.Owner{},
-				createMaterailzationRequest: &v1alpha.CreateMaterializationRequest{},
-			},
-			want:    nil,
-			wantErr: true,
-			err:     fmt.Errorf("missing materialization definition"),
-		},
-		{
-			name: "missing expression should throw error",
-			fields: fields{
-				UnimplementedMaterializationServiceServer: v1alpha.UnimplementedMaterializationServiceServer{},
-			},
-			args: args{
-				owner: &ent.Owner{},
-				createMaterailzationRequest: &v1alpha.CreateMaterializationRequest{
+	BeforeEach(func() {
+		owner = &ent.Owner{}
+	})
+
+	Context("CreateMaterialization", func() {
+		Context("missing materialization", func() {
+
+			It("should throw error", func() {
+				materializationService := &materializationService{
+					UnimplementedMaterializationServiceServer: v1alpha.UnimplementedMaterializationServiceServer{},
+				}
+				response, err := materializationService.createMaterialization(context.Background(), owner, &v1alpha.CreateMaterializationRequest{})
+				Expect(err).Should(HaveOccurred())
+				Expect(err.Error()).Should(Equal("missing materialization definition"))
+				Expect(response).Should(BeNil())
+			})
+		})
+
+		Context("missing expression", func() {
+			It("should throw error", func() {
+				materializationService := &materializationService{
+					UnimplementedMaterializationServiceServer: v1alpha.UnimplementedMaterializationServiceServer{},
+				}
+				response, err := materializationService.createMaterialization(context.Background(), owner, &v1alpha.CreateMaterializationRequest{
 					Materialization: &v1alpha.Materialization{
 						Expression:  "",
 						Destination: objectStoreDestination,
 					},
-				},
-			},
-			want:    nil,
-			wantErr: true,
-			err:     fmt.Errorf("missing materialization expression"),
-		},
-		{
-			name: "missing destination should throw error",
-			fields: fields{
-				UnimplementedMaterializationServiceServer: v1alpha.UnimplementedMaterializationServiceServer{},
-			},
-			args: args{
-				owner: &ent.Owner{},
-				createMaterailzationRequest: &v1alpha.CreateMaterializationRequest{
+				})
+				Expect(err).Should(HaveOccurred())
+				Expect(err.Error()).Should(Equal("missing materialization expression"))
+				Expect(response).Should(BeNil())
+			})
+		})
+
+		Context("missing destination", func() {
+			It("should throw error", func() {
+				materializationService := &materializationService{
+					UnimplementedMaterializationServiceServer: v1alpha.UnimplementedMaterializationServiceServer{},
+				}
+
+				response, err := materializationService.createMaterialization(context.Background(), owner, &v1alpha.CreateMaterializationRequest{
 					Materialization: &v1alpha.Materialization{
 						Expression: "nachos",
 					},
-				},
-			},
-			want:    nil,
-			wantErr: true,
-			err:     fmt.Errorf("missing materialization destination"),
-		},
-		{
-			name: "compile error should throw error",
-			fields: fields{
-				UnimplementedMaterializationServiceServer: v1alpha.UnimplementedMaterializationServiceServer{},
-				computeManager: compute.NewComputeManagerMock(compute.GetComputeManagerMockConfig()),
-			},
-			args: args{
-				owner: &ent.Owner{},
-				createMaterailzationRequest: &v1alpha.CreateMaterializationRequest{
-					Materialization: &v1alpha.Materialization{
-						Expression:  "nachos",
-						Destination: pulsarDestination,
-					},
-				},
-			},
-			want:    nil,
-			wantErr: true,
-			err:     fmt.Errorf("missing materialization destination"),
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			q := &materializationService{
-				UnimplementedMaterializationServiceServer: tt.fields.UnimplementedMaterializationServiceServer,
-				kaskadaTableClient:                        tt.fields.kaskadaTableClient,
-				kaskadaViewClient:                         tt.fields.kaskadaViewClient,
-				dataTokenClient:                           tt.fields.dataTokenClient,
-				materializationClient:                     tt.fields.materializationClient,
-				computeManager:                            tt.fields.computeManager,
-			}
-			ctx := context.Background()
-			got, err := q.createMaterialization(ctx, tt.args.owner, tt.args.createMaterailzationRequest)
-			if err != nil {
-				if tt.wantErr {
-					if err.Error() != tt.err.Error() {
-						t.Errorf("materializationService.CreateMaterialization() error = %v, want %v", err, tt.err)
-					}
-				} else {
-					t.Errorf("materializationService.CreateMaterialization() error = %v, wantErr %v", err, tt.err)
-					return
-				}
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("materializationService.CreateMaterialization() = %v, want %v", got, tt.want)
-			}
+				})
+				Expect(err).Should(HaveOccurred())
+				Expect(err.Error()).Should(Equal("missing materialization destination"))
+				Expect(response).Should(BeNil())
+			})
 		})
-	}
-}
+
+		Context("the compiled query includes both pulsar and object store sources", func() {
+			It("should throw error", func() {
+				mockComputeManager := compute.NewMockComputeManager(GinkgoT())
+				expectedCompileResponse := v1alpha.CompileResponse{
+					MissingNames:    []string{},
+					FenlDiagnostics: nil,
+					Plan:            &v1alpha.ComputePlan{},
+					FreeNames:       []string{"file_backed_source", "pulsar_backed_source"},
+				}
+				mockComputeManager.EXPECT().CompileQuery(mock.Anything, owner, "nachos", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&expectedCompileResponse, nil)
+
+			})
+		})
+
+		It("works without error", func() {
+			mockComputeManager := compute.NewMockComputeManager(GinkgoT())
+			expectedCompileResponse := v1alpha.CompileResponse{}
+
+			mockComputeManager.EXPECT().CompileQuery(mock.Anything, owner, "nachos", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&expectedCompileResponse, nil)
+
+			materializationService := &materializationService{
+				UnimplementedMaterializationServiceServer: v1alpha.UnimplementedMaterializationServiceServer{},
+				computeManager: mockComputeManager,
+			}
+			response, err := materializationService.createMaterialization(context.Background(), owner, &v1alpha.CreateMaterializationRequest{
+				Materialization: &v1alpha.Materialization{
+					Expression:  "nachos",
+					Destination: pulsarDestination,
+				},
+			})
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(response).ShouldNot(BeNil())
+			Expect(response.Analysis).ShouldNot(BeNil())
+			Expect(response.Analysis.CanExecute).Should(BeTrue())
+			Expect(response.Materialization).ShouldNot(BeNil())
+
+		})
+	})
+
+})
---
+++ wren/service/service_suite_test.go
@@ -0,0 +1,14 @@
+package service_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestService(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Service Suite")
+}
+
