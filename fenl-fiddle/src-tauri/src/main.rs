62785648b95b0b90f329c19c93755933a99a5533
Eric Pinzur <epinzur@gmail.com>
2023-06-16 05:01:10 +0200

    can now get_schema

--- fenl-fiddle/src-tauri/src/main.rs
+++ fenl-fiddle/src-tauri/src/main.rs
@@ -1,16 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::Arc;
+
+use error_stack::{IntoReport, ResultExt};
 use sparrow_api::kaskada::v1alpha::Schema;
 use sparrow_api::kaskada::v1alpha::{
-    GetMetadataRequest, GetMetadataResponse, MergeMetadataRequest, MergeMetadataResponse,
     SourceData, SourceMetadata,
 };
+use sparrow_api::kaskada::v1alpha::source_data::Source::CsvData;
 
 use sparrow_runtime::RawMetadata;
 
 use sparrow_runtime::stores::ObjectStoreRegistry;
-use tonic::Response;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -18,9 +20,17 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-fn get_schema(csv: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", csv)
+#[tauri::command(async)]
+async fn get_schema(csv: String) -> String {
+    let object_store_registry = Arc::new(ObjectStoreRegistry::new());
+
+    let source_data = SourceData {
+        source: Some(CsvData(csv.clone()))
+    };
+    let result = get_source_metadata(&object_store_registry, &source_data)
+            .await
+            .unwrap();
+    format!("{:?}", result)
 }
 
 
@@ -32,3 +42,40 @@ fn main() {
         .expect("error while running tauri application");
 }
 
+#[derive(derive_more::Display, Debug)]
+enum Error {
+    #[display(fmt = "unable to get source path from request")]
+    SourcePath,
+    #[display(fmt = "schema error: '{_0}'")]
+    Schema(String),
+}
+impl error_stack::Context for Error {}
+
+async fn get_source_metadata(
+    object_store_registry: &ObjectStoreRegistry,
+    source: &SourceData,
+) -> error_stack::Result<SourceMetadata, Error> {
+    let source = source.source.as_ref().ok_or(Error::SourcePath)?;
+    let metadata = RawMetadata::try_from(source, object_store_registry)
+        .await
+        .attach_printable_lazy(|| format!("Source: {:?}", source))
+        .change_context(Error::Schema(format!(
+            "unable to read schema from: {:?}",
+            source
+        )))?;
+    let schema = Schema::try_from(metadata.table_schema.as_ref())
+        .into_report()
+        .attach_printable_lazy(|| {
+            format!(
+                "Raw Schema: {:?} Table Schema: {:?}",
+                metadata.raw_schema, metadata.table_schema
+            )
+        })
+        .change_context(Error::Schema(format!(
+            "Unable to encode schema {:?} for source file {:?}",
+            metadata.table_schema, source
+        )))?;
+    Ok(SourceMetadata {
+        schema: Some(schema),
+    })
+}
