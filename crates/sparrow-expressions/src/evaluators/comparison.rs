3796009b248c48c54ceec9d91f64696cf67d75f7
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2023-06-30 16:10:55 -0700

    comments

--- crates/sparrow-expressions/src/evaluators/comparison.rs
+++ crates/sparrow-expressions/src/evaluators/comparison.rs
@@ -1,8 +1,4 @@
 mod gt_primitive;
-mod gt_primitive_scalar;
 mod gte_primitive;
-mod gte_primitive_scalar;
 mod lt_primitive;
-mod lt_primitive_scalar;
 mod lte_primitive;
-mod lte_primitive_scalar;
