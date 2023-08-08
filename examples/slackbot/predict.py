4de6596872cbccdb3fd2f74533c017ed0d5c70bb
Eric Pinzur <epinzur@gmail.com>
2023-08-08 20:43:20 +0200

    more stuff

--- examples/slackbot/predict.py
+++ examples/slackbot/predict.py
@@ -7,10 +7,11 @@ next_message_model = 'ada:ft-personal:next-message-user-full-kaskada-2023-08-03-
 # i think this model was mistakenly trained with the next_users data
 next_reaction_model = 'ada:ft-personal:next-reaction-full-kaskada-2023-08-03-17-03-13'
 next_users_model = 'ada:ft-personal:next-users-in-window-full-kaskada-2023-08-03-18-17-36'
+coversations_model = "davinci:ft-personal:coversation-users-full-kaskada-2023-08-05-14-25-30"
 
-in_file_name = 'next_reaction_valid'
-model = next_reaction_model
-stop = " END"
+in_file_name = 'conversation_user_examples_prepared_valid'
+model = coversations_model
+stop = " end"
 
 
 df = pandas.read_json(f'{work_dir}/{in_file_name}.jsonl', lines=True)
@@ -18,10 +19,14 @@ prompts = df['prompt']
 total = len(prompts)
 items = [(i, prompts[i]) for i in range(total)]
 
+df['prediction'] = None
+
 # task that operates on an item
 def task(index, prompt):
-    res = openai.Completion.create(model=next_users_model, prompt=prompt, max_tokens=50, stop=stop)
-    pred = res['choices'][0]['text']
+    if len(prompt) > 5000:
+        return (index, {})
+    pred = openai.Completion.create(model=next_users_model, prompt=prompt, max_tokens=1, stop=' end', n=1, logprobs=5, temperature=0)
+    # pred = pred['choices'][0]['text']
     return (index, pred)
 
 if __name__ == '__main__':
@@ -31,4 +36,3 @@ if __name__ == '__main__':
             df.at[index, 'prediction'] = pred
 
 df.to_json(f'{work_dir}/{in_file_name}_with_pred.jsonl', lines=True, orient='records')
-
