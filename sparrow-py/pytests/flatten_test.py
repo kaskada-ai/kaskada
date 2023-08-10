2e1cf23f77afac5f0aa82108fb5f2b1047ca8ec2
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2023-08-09 21:20:42 -0700

    feat: Add union operation (#642)

--- sparrow-py/pytests/flatten_test.py
+++ sparrow-py/pytests/flatten_test.py
@@ -1,23 +1,16 @@
-import pytest
 import sparrow_py as kt
 
-
-@pytest.fixture(scope="module")
-def source() -> kt.sources.CsvString:
-    content = "\n".join(
+def test_flatten(golden) -> None:
+    source = kt.sources.PyList(
         [
-            "time,key,m,n",
-            "1996-12-19T16:39:57,A,5,10",
-            "1996-12-19T16:39:58,B,24,3",
-            "1996-12-19T16:39:59,A,17,6",
-            "1996-12-19T16:40:00,A,,9",
-            "1996-12-19T16:40:01,A,12,",
-            "1996-12-19T16:40:02,A,,",
-        ]
+            { "time": "1996-12-19T16:39:57", "user": "A", "m": [ [ 5 ] ] },
+            { "time": "1996-12-19T17:39:57", "user": "A", "m": [ ] },
+            { "time": "1996-12-19T18:39:57", "user": "A", "m": [ None ] },
+            { "time": "1996-12-19T19:39:57", "user": "A", "m": [ [6], [7] ] },
+            { "time": "1996-12-19T19:39:57", "user": "A", "m": [ [7, 8], [9, 10] ] },
+        ],
+        time_column_name="time",
+        key_column_name="user",
     )
-    return kt.sources.CsvString(content, time_column_name="time", key_column_name="key")
 
-
-@pytest.mark.skip(reason="no way of currently testing this")
-def test_flatten(source, golden) -> None:
-    pass
+    golden.jsonl(source.col("m").flatten())
