8c6af843abdc5e9f1143a3485c17112505fa7c8a
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2023-08-16 14:17:49 -0700

    fix times

--- python/pytests/pandas_source_test.py
+++ python/pytests/pandas_source_test.py
@@ -1,28 +1,35 @@
 import kaskada as kd
 import pandas as pd
 
-def test_float_microseconds(golden) -> None:
+def test_float_seconds(golden) -> None:
     data = {
         'time': [1671477472.026119],
         'user': ['tom']
     }
     df = pd.DataFrame(data)
-    table = kd.sources.Pandas(df, time_column_name="time", key_column_name="user", time_unit="us")
+    table = kd.sources.Pandas(df, time_column_name="time", key_column_name="user", time_unit="s")
+
+    golden.jsonl(table)
+
+def test_float_milliseconds(golden) -> None:
+    data = {
+        'time': [1671477472026.119],
+        'user': ['tom']
+    }
+    df = pd.DataFrame(data)
+    table = kd.sources.Pandas(df, time_column_name="time", key_column_name="user", time_unit="ms")
 
     golden.jsonl(table)
 
 def test_float_nanoseconds(golden) -> None:
     data = {
-        'time': [1671477472026.119],
+        'time': [1671477472026119000],
         'user': ['tom']
     }
     df = pd.DataFrame(data)
     table = kd.sources.Pandas(df, time_column_name="time", key_column_name="user")
 
     golden.jsonl(table)
-
-def test_add_dataframe(golden) -> None:
-    pass
 
 def test_add_dataframe(golden) -> None:
     df1 = pd.DataFrame({
