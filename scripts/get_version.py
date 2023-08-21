1752793197e3910f28290331c8b728d564cc5e69
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2023-08-21 14:00:14 -0700

    feat: Setup release process (#685)
    This should do the following:
    
    1. Use `release-drafter` to maintain a draft of a `v###` release.
    2. Build wheels with a version based on either the current state of the
    file or using the version from the tag.
    3. When we publish that release, it should publish to PyPi.

---
+++ scripts/get_version.py
@@ -0,0 +1,28 @@
+import tomlkit
+import argparse
+from typing import List
+
+def get_value_from_toml(file_path: str, toml_path: List[str]) -> str:
+    """Retrieve a value from a TOML file at the given path."""
+    with open(file_path, 'r') as f:
+        data = tomlkit.parse(f.read())
+
+    temp = data
+    for key in toml_path:
+        temp = temp[key]
+
+    return str(temp)  # Convert value to string in case it's a number or boolean
+
+def main():
+    parser = argparse.ArgumentParser(description='Retrieve value from a TOML file.')
+    parser.add_argument('file', type=str, help='Path to the TOML file.')
+    parser.add_argument('path', type=str, help='Path within the TOML file (e.g., package.version)')
+
+    args = parser.parse_args()
+
+    toml_path = args.path.split('.')
+    value = get_value_from_toml(args.file, toml_path)
+    print(f"Value at '{args.path}' in '{args.file}': {value}")
+
+if __name__ == "__main__":
+    main()
