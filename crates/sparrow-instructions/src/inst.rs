b946b6723d1ec400bb8828c0aff9089d59c415cd
Jordan Frazier <jordan.frazier@datastax.com>
2023-08-22 13:35:45 -0700

    fix hash again

--- crates/sparrow-instructions/src/inst.rs
+++ crates/sparrow-instructions/src/inst.rs
@@ -264,11 +264,13 @@ impl PartialEq for InstKind {
 
 impl Hash for InstKind {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        core::mem::discriminant(self).hash(state);
         match self {
             InstKind::Udf(udf) => udf.hash(state),
             InstKind::Simple(op) => op.hash(state),
             InstKind::Cast(dt) => dt.hash(state),
-            _ => core::mem::discriminant(self).hash(state),
+            InstKind::Record => {}
+            InstKind::FieldRef => {}
         }
     }
 }
