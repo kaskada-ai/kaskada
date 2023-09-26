9cd4a9eeadacb3c9b298bd839b77f438761aad21
Jordan Frazier <jordan.frazier@datastax.com>
2023-09-26 15:28:15 -0700

    Remove column behavior and use new prepare batch

--- crates/sparrow-runtime/src/prepare/prepare_input_stream.rs
+++ crates/sparrow-runtime/src/prepare/prepare_input_stream.rs
@@ -1,23 +1,21 @@
 use std::borrow::BorrowMut;
+use std::sync::atomic::AtomicU64;
 
 use anyhow::Context;
 use arrow::array::{ArrayRef, UInt64Array};
-use arrow::compute::SortColumn;
-use arrow::datatypes::{ArrowPrimitiveType, TimestampNanosecondType};
 use arrow::record_batch::RecordBatch;
 use error_stack::{IntoReport, IntoReportCompat, ResultExt};
 use futures::stream::BoxStream;
 use futures::StreamExt;
-use itertools::Itertools;
 use sparrow_api::kaskada::v1alpha::{slice_plan, TableConfig};
 use sparrow_arrow::downcast::downcast_primitive_array;
 use sparrow_core::TableSchema;
 
 use crate::prepare::slice_preparer::SlicePreparer;
 use crate::prepare::Error;
+use crate::preparer::prepare_batch;
 use crate::RawMetadata;
 
-use super::column_behavior::ColumnBehavior;
 use super::PrepareMetadata;
 
 /// Creates a stream over unordered, unprepared batches that is responsible
@@ -30,7 +28,7 @@ use super::PrepareMetadata;
 /// 4. Computing the key-hash and key batch metadata.
 pub async fn prepare_input<'a>(
     mut reader: BoxStream<'a, error_stack::Result<RecordBatch, Error>>,
-    config: &TableConfig,
+    config: &'a TableConfig,
     raw_metadata: RawMetadata,
     prepare_hash: u64,
     slice: &Option<slice_plan::Slice>,
@@ -39,39 +37,6 @@ pub async fn prepare_input<'a>(
     // to manually do that (as part of deprecating `TableSchema`)?
     let prepared_schema = TableSchema::try_from_data_schema(raw_metadata.table_schema.clone())?;
     let prepared_schema = prepared_schema.schema_ref().clone();
-
-    // Add column behaviors for each of the 3 key columns.
-    let mut columns = Vec::with_capacity(prepared_schema.fields().len());
-    columns.push(ColumnBehavior::try_new_cast(
-        &raw_metadata.raw_schema,
-        &config.time_column_name,
-        &TimestampNanosecondType::DATA_TYPE,
-        false,
-    )?);
-    if let Some(subsort_column_name) = &config.subsort_column_name {
-        columns.push(ColumnBehavior::try_new_subsort(
-            &raw_metadata.raw_schema,
-            subsort_column_name,
-        )?);
-    } else {
-        columns.push(ColumnBehavior::try_default_subsort(prepare_hash)?);
-    }
-
-    columns.push(ColumnBehavior::try_new_entity_key(
-        &raw_metadata.raw_schema,
-        &config.group_column_name,
-        false,
-    )?);
-
-    // Add column behaviors for each column.  This means we include the key columns
-    // redundantly, but cleaning that up is a big refactor.
-    // See https://github.com/riptano/kaskada/issues/90
-    for field in raw_metadata.table_schema.fields() {
-        columns.push(ColumnBehavior::try_cast_or_reference_or_null(
-            &raw_metadata.raw_schema,
-            field,
-        )?);
-    }
 
     // we've already checked that the group column exists so we can just unwrap it here
     let (entity_column_index, entity_key_column) = raw_metadata
@@ -85,65 +50,23 @@ pub async fn prepare_input<'a>(
     )?;
 
     let mut metadata = PrepareMetadata::new(entity_key_column.data_type().clone());
-
+    let next_subsort = AtomicU64::new(prepare_hash);
     Ok(async_stream::try_stream! {
         while let Some(Ok(batch)) = reader.next().await {
             // 1. Slicing may reduce the number of entities to operate and sort on.
             let read_batch = slice_preparer.slice_batch(batch)?;
-            // 2. Prepare each of the columns by getting the column behavior result
-            let mut prepared_columns = Vec::new();
-            for c in columns.iter_mut() {
-                let result = c
-                    .get_result(&read_batch)
-                    .await?;
 
-                // update_key_metadata(c, &read_batch, &result, &mut metadata)?;
-                prepared_columns.push(result);
-            }
+            // 2. Prepare the batch
+            let prepared_batch = prepare_batch(&read_batch, config, prepared_schema.clone(), &next_subsort, None).unwrap();
 
-            // 3. Update the key hash mappings
-            let key_column = read_batch.column(entity_column_index);
-            let key_hashes = prepared_columns.get(2).expect("key column");
-            update_key_metadata(key_column, key_hashes, &mut metadata)?;
+            // 3. Update the key inverse
+            let key_hash_column = prepared_batch.column(2);
+            let key_column = prepared_batch.column(entity_column_index + 3);
+            update_key_metadata(key_column, key_hash_column, &mut metadata)?;
 
-            // 4. Pull out the time, subsort and key hash columns to sort the record batch
-            let time_column = &prepared_columns[0];
-            let subsort_column = &prepared_columns[1];
-            let key_hash_column = &prepared_columns[2];
-            let sorted_indices = arrow::compute::lexsort_to_indices(
-                &[
-                    SortColumn {
-                        values: time_column.clone(),
-                        options: None,
-                    },
-                    SortColumn {
-                        values: subsort_column.clone(),
-                        options: None,
-                    },
-                    SortColumn {
-                        values: key_hash_column.clone(),
-                        options: None,
-                    },
-                ],
-                None,
-            )
-            .into_report()
-            .change_context(Error::SortingBatch)?;
-
-            // 5. Produce the fully ordered record batch by taking the indices out from the
-            // columns
-            let prepared_columns: Vec<_> = prepared_columns
-                .iter()
-                .map(|column| arrow::compute::take(column.as_ref(), &sorted_indices, None))
-                .try_collect()
-                .into_report()
-                .change_context(Error::Internal)?;
-
-            let batch = RecordBatch::try_new(prepared_schema.clone(), prepared_columns.clone())
-                .into_report()
-                .change_context(Error::Internal)?;
+            // 4. Produce the batch and associated metadata
             let metadata = metadata.get_flush_metadata()?;
-            let result = (batch, metadata);
+            let result = (prepared_batch, metadata);
             yield result;
         }
     }
