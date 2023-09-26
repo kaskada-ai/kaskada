ac280b5ab6696f7937602b997b1ce346431f19ac
Eric Pinzur <2641606+epinzur@users.noreply.github.com>
2023-09-26 10:58:29 +0200

    fixed issue when subsort is provided (#771)

--- python/pytests/jsonl_string_source_test.py
+++ python/pytests/jsonl_string_source_test.py
@@ -34,3 +34,22 @@ async def test_read_jsonl_lists(golden) -> None:
         """
     )
     golden.jsonl(source)
+
+
+async def test_read_jsonl_with_subsort(golden) -> None:
+    source = await kd.sources.JsonlString.create(
+        '{"time": "1996-12-19T16:39:57", "user": "A", "m": 5, "n": 10, "subsort": 1}',
+        time_column="time",
+        key_column="user",
+        subsort_column="subsort",
+    )
+    golden.jsonl(source)
+
+    await source.add_string(
+        """
+        {"time": "1996-12-19T16:40:57", "user": "A", "m": 8, "n": 10, "subsort": 2}
+        {"time": "1996-12-19T16:41:57", "user": "B", "m": 5, "subsort": 3}
+        """
+    )
+
+    golden.jsonl(source)
--- python/pytests/parquet_source_test.py
+++ python/pytests/parquet_source_test.py
@@ -11,3 +11,16 @@ async def test_read_parquet(golden) -> None:
 
     await source.add_file("../testdata/purchases/purchases_part2.parquet")
     golden.jsonl(source)
+
+
+async def test_read_parquet_with_subsort(golden) -> None:
+    source = await kd.sources.Parquet.create(
+        "../testdata/purchases/purchases_part1.parquet",
+        time_column="purchase_time",
+        key_column="customer_id",
+        subsort_column="subsort_id",
+    )
+    golden.jsonl(source)
+
+    await source.add_file("../testdata/purchases/purchases_part2.parquet")
+    golden.jsonl(source)
