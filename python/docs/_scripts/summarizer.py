c5fe17a0f48b57a4a08a2e7a08972a1c09fe4a86
Eric Pinzur <2641606+epinzur@users.noreply.github.com>
2023-10-10 17:39:11 +0200

    made some quarto tweaks (#801)
    * removed 1-element sidebar from Community page
    * re-added `docs` to linting
    * Setup overrides for error F811 for reference generation scripts.
    https://www.flake8rules.com/rules/F811.html
    * Fixed other linting issues for reference generation scripts

--- python/docs/_scripts/summarizer.py
+++ python/docs/_scripts/summarizer.py
@@ -16,16 +16,16 @@ class Summarizer:
         return f"| {link} | {description} |"
 
     @dispatch
-    def summarize(self, el):
+    def summarize(self, el):  # noqa: F811
         raise NotImplementedError(f"Unsupported type: {type(el)}")
 
     @dispatch
-    def summarize(self, el: layout.Layout):
+    def summarize(self, el: layout.Layout):  # noqa: F811
         rendered_sections = list(map(self.summarize, el.sections))
         return "\n\n".join(rendered_sections)
 
     @dispatch
-    def summarize(self, el: layout.Section):
+    def summarize(self, el: layout.Section):  # noqa: F811
         desc = f"\n\n{el.desc}" if el.desc is not None else ""
         if el.title is not None:
             header = f"## {el.title}{desc}"
@@ -40,7 +40,7 @@ class Summarizer:
         return header
 
     @dispatch
-    def summarize(self, contents: layout.ContentList):
+    def summarize(self, contents: layout.ContentList):  # noqa: F811
         thead = "| | |\n| --- | --- |"
 
         rendered = []
@@ -50,7 +50,7 @@ class Summarizer:
         return "\n".join([thead, *rendered])
 
     @dispatch
-    def summarize(self, el: layout.Page):
+    def summarize(self, el: layout.Page):  # noqa: F811
         if el.summary is not None:
             # TODO: assumes that files end with .qmd
             return self._summary_row(
@@ -68,18 +68,18 @@ class Summarizer:
             return "\n".join(rows)
 
     @dispatch
-    def summarize(self, el: layout.MemberPage):
+    def summarize(self, el: layout.MemberPage):  # noqa: F811
         # TODO: model should validate these only have a single entry
         return self.summarize(el.contents[0], el.path, shorten=True)
 
     @dispatch
-    def summarize(self, el: layout.Interlaced, *args, **kwargs):
+    def summarize(self, el: layout.Interlaced, *args, **kwargs):  # noqa: F811
         rows = [self.summarize(doc, *args, **kwargs) for doc in el.contents]
 
         return "\n".join(rows)
 
     @dispatch
-    def summarize(
+    def summarize(  # noqa: F811
         self, el: layout.Doc, path: Optional[str] = None, shorten: bool = False
     ):
         # this is where summary page method links are created
@@ -93,12 +93,12 @@ class Summarizer:
         return self._summary_row(link, description)
 
     @dispatch
-    def summarize(self, el: layout.Link):
+    def summarize(self, el: layout.Link):  # noqa: F811
         description = self.summarize(el.obj)
         return self._summary_row(f"[](`{el.name}`)", description)
 
     @dispatch
-    def summarize(self, obj: Union[dc.Object, dc.Alias]) -> str:
+    def summarize(self, obj: Union[dc.Object, dc.Alias]) -> str:  # noqa: F811
         """Test"""
         # get high-level description
         doc = obj.docstring
