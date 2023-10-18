1235c69720964b534dffa68970964b132eb40cbf
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2023-10-18 13:54:44 -0700

    add inputs

--- python/pytests/math_test.py
+++ python/pytests/math_test.py
@@ -40,81 +40,81 @@ async def source(request) -> SourceCols:
     return SourceCols(m=source.col("m"), n=source.col("n"), datatype=datatype)
 
 async def test_add(source, golden) -> None:
-    golden.jsonl(source.m + source.n)
+    golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m + source.n}))
 
 async def test_add_literal(source, golden) -> None:
-    golden.jsonl(source.m + 1)
+    golden.jsonl(kd.record({"m": source.m, "result": source.m + 1}))
 
 async def test_sub(source, golden) -> None:
     if pa.types.is_unsigned_integer(source.datatype):
         pytest.skip("unsigned integers currently underflows")
 
-    golden.jsonl(source.m - source.n)
+    golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m - source.n}))
 
 async def test_sub_literal(source, golden) -> None:
-    golden.jsonl(source.m - 1)
+    golden.jsonl(kd.record({"m": source.m, "result": source.m - 1}))
 
 async def test_mul(source, golden) -> None:
     if source.datatype == pa.int8():
         pytest.skip("int8 currently overflows on multiplication")
-    golden.jsonl(source.m * source.n)
+    golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m * source.n}))
 
 async def test_mul_literal(source, golden) -> None:
-    golden.jsonl(source.m * 2)
+    golden.jsonl(kd.record({"m": source.m, "result": source.m * 2}))
 
 async def test_div(source, golden) -> None:
-    golden.jsonl(source.m / source.n)
+    golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m / source.n}))
 
 async def test_div_literal(source, golden) -> None:
-    golden.jsonl(source.m / 2)
+    golden.jsonl(kd.record({"m": source.m, "result": source.m / 2}))
 
 async def test_neg(source, golden) -> None:
-    golden.jsonl(source.m.neg())
+    golden.jsonl(kd.record({"m": source.m, "result": source.m.neg()}))
 
 async def test_ceil(source, golden) -> None:
-    golden.jsonl(source.m.ceil())
+    golden.jsonl(kd.record({"m": source.m, "result": source.m.ceil()}))
 
 async def test_floor(source, golden) -> None:
     golden.jsonl(source.m.floor())
 
 async def test_round(source, golden) -> None:
-    golden.jsonl(source.m.round())
+    golden.jsonl(kd.record({"m": source.m, "result": source.m.round()}))
 
 async def test_greatest(source, golden) -> None:
-    golden.jsonl(source.m.greatest(source.n))
+    golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m.greatest(source.n)}))
 
 async def test_greatest_literal(source, golden) -> None:
-    golden.jsonl(source.m.greatest(8))
+    golden.jsonl(kd.record({"m": source.m, "result": source.m.greatest(8)}))
 
 async def test_least(source, golden) -> None:
-    golden.jsonl(source.m.least(source.n))
+    golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m.least(source.n)}))
 
 async def test_least_literal(source, golden) -> None:
-    golden.jsonl(source.m.least(4))
+    golden.jsonl(kd.record({"m": source.m, "result": source.m.least(4)}))
 
 async def test_powf(source, golden) -> None:
-    golden.jsonl(source.m.powf(source.n))
+    golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m.powf(source.n)}))
 
 async def test_powf_literal(source, golden) -> None:
-    golden.jsonl(source.m.powf(2))
+    golden.jsonl(kd.record({"m": source.m, "result": source.m.powf(2)}))
 
 async def test_exp(source, golden) -> None:
-    golden.jsonl(source.m.exp())
+    golden.jsonl(kd.record({"m": source.m, "result": source.m.exp()}))
 
 async def test_clamp_min(source, golden) -> None:
-    golden.jsonl(source.m.clamp(min=source.n))
+    golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m.clamp(min=source.n)}))
 
 async def test_clamp_max(source, golden) -> None:
-    golden.jsonl(source.m.clamp(min=source.n))
+    golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m.clamp(max=source.n)}))
 
 async def test_clamp_min_literal_max(source, golden) -> None:
-    golden.jsonl(source.m.clamp(min=source.n, max=8))
+        golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m.clamp(min=source.n, max = 8)}))
 
 async def test_clamp_max_literal_min(source, golden) -> None:
-    golden.jsonl(source.m.clamp(min=0, max=source.n))
+        golden.jsonl(kd.record({"m": source.m, "n": source.n, "result": source.m.clamp(min=0, max=source.n)}))
 
 async def test_clamp_literal(source, golden) -> None:
-    golden.jsonl(source.m.clamp(min=0, max=10))
+    golden.jsonl(kd.record({"m": source.m, "result": source.m.clamp(min=0, max=10)}))
 
 async def test_sqrt(source, golden) -> None:
-    golden.jsonl(source.m.sqrt())
+    golden.jsonl(kd.record({"m": source.m, "result": source.m.sqrt()}))
